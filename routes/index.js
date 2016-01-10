e0f0f3719c0266ddcf99b49ecf20f32c726abc57
Stephen Mayeux <stephenmayeux@gmail.com>
2016-01-10 22:12:45 +0900

    fetch unix and serve json

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Timestamp Microservice' });
 });
 
+router.get('/:time', function(req, res, next) {
+  var time = new Date(req.params.time * 1000),
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    month = months[time.getMonth()],
+    date = time.getDate(),
+    year = time.getFullYear(),
+    naturalDate = month + ' ' + date + ', ' + year,
+    data = {
+      unix: req.params.time,
+      natural: naturalDate
+    };
+    console.log(month + ' ' + date + ', ' + year);
+  res.json(data);
+});
+
 module.exports = router;
