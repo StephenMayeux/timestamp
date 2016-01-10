897ea0fdc2950bf494629aab1a4ffbf82c3047ec
Stephen Mayeux <stephenmayeux@gmail.com>
2016-01-11 02:06:24 +0900

    convert natural to unix

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,43 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:time', function(req, res, next) {
-  var time = new Date(req.params.time * 1000),
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    month = months[time.getMonth()],
-    date = time.getDate(),
-    year = time.getFullYear(),
-    naturalDate = month + ' ' + date + ', ' + year,
-    data = {
-      unix: req.params.time,
-      natural: naturalDate
-    };
-    console.log(month + ' ' + date + ', ' + year);
-  res.json(data);
+
+  function unixToNatural(unix) {
+    var time = new Date(unix * 1000),
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        month = months[time.getMonth()],
+        date = time.getDate(),
+        year = time.getFullYear(),
+        naturalDate = month + ' ' + date + ', ' + year,
+        data = {
+          unix: time / 1000,
+          natural: naturalDate
+        };
+
+        return data;
+  }
+
+
+  // checks if URL parameter contains numbers only
+  if (!isNaN(req.params.time)) {
+    var result = unixToNatural(req.params.time);
+    res.json(result);
+  } else {
+    // checks if URL parameter contains valid date
+    if (!isNaN(new Date(req.params.time))) {
+      var d = new Date(req.params.time),
+          x = d.getFullYear(),
+          y = d.getMonth(),
+          z = d.getDate(),
+          utc = new Date(Date.UTC(x,y,z)),
+          unix = Date.parse(utc) / 1000;
+
+      var result2 = unixToNatural(unix);
+      res.json(result2);
+    } else {
+      res.json({unix: null, natural: null});
+    }
+  }
 });
 
 module.exports = router;
